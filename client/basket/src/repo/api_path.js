40ff8ea4890eedb831c82a5e1921b079374d9668
Santhi Ranimekala <santhiranimekala025@gmail.com>
2026-06-07 15:24:02 +0530

    first commit

---
+++ client/basket/src/repo/api_path.js
@@ -0,0 +1,7 @@
+
+
+export const productUrl="http://localhost:3000/api"
+export const imageUrl="http://localhost:3000"
+export const adminUrl="http://localhost:3000/admin"
+export const emailUrl="http://localhost:3000/email"
+export const cartUrl="http://localhost:3000/cart"
